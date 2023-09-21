ed7478eb7861c09d6efb54d693071b440407e2ec
smouuuuuk <alexander.cabellop@gmail.com>
2023-09-21 19:40:14 +0200

    Muchamierda updated

--- pages/proyectos/mucha-mierda.js
+++ pages/proyectos/mucha-mierda.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Layout from "../../components/layout";
-import styles from '../../styles/UGC-Marca.module.css';
+import styles from '../../styles/Muchamierda.module.css';
 import textStyles from '../../styles/Textos.module.css';
 import React, { useEffect, useState } from "react"
 import { useKeenSlider } from "keen-slider/react"
@@ -8,6 +8,26 @@ import "keen-slider/keen-slider.min.css"
 
 
 export default function MuchaMierda(){
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+    const [sliderRef, instanceRef] = useKeenSlider({
+        initial: 0,
+        loop: true,
+        slides: {
+            perView: 1
+        },
+        defaultAnimation: {
+            duration: 1500
+        },
+        slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel)
+        },
+        created() {
+        setLoaded(true)
+        },
+    })
+
     return(
         <Layout donde="proyecto">
             <div id={styles.todo}>
@@ -41,13 +61,90 @@ export default function MuchaMierda(){
                 </div>
 
                 <div id={ styles.imagenes }>
-                    <video id={ styles.videougc } width="1600" autoPlay={true} loop={true} muted={true} playsInline={true} style={{ borderRadius:"35px" }}><source src="/images/un-gusto-conocerte-marca/logo.mp4" type="video/mp4"/></video>
-                    <div id={styles.tote_gif}>
+                    <Image 
+                        className= { styles.imagen }
+                        priority
+                        src="/images/home/home_3.jpg"
+                        width={ 1600 }
+                        height={ 1600 }
+                        alt="Todos los estuches de conservas"
+                    />
+                    <div className={ `${styles.imagen} navigation-wrapper` }>
+                        <div ref={sliderRef} className="keen-slider">
+                            <div className="keen-slider__slide">
+                                <Image
+                                    priority
+                                    src="/images/mucha-mierda/carrusel_1.jpg"
+                                    width={ 1600 }
+                                    height={ 1600 }
+                                    alt=""
+                                />
+                            </div>
+                            <div className="keen-slider__slide">
+                                <Image
+                                    priority
+                                    src="/images/mucha-mierda/carrusel_2.jpg"
+                                    width={ 1600 }
+                                    height={ 1600 }
+                                    alt=""
+                                />
+                            </div>
+                            <div className="keen-slider__slide">
+                                <Image
+                                    priority
+                                    src="/images/mucha-mierda/carrusel_3.jpg"
+                                    width={ 1600 }
+                                    height={ 1600 }
+                                    alt=""
+                                />
+                            </div>
+                            <div className="keen-slider__slide">
+                                <Image
+                                    priority
+                                    src="/images/mucha-mierda/carrusel_4.jpg"
+                                    width={ 1600 }
+                                    height={ 1600 }
+                                    alt=""
+                                />
+                            </div>
+                            <div className="keen-slider__slide">
+                                <Image
+                                    priority
+                                    src="/images/mucha-mierda/carrusel_5.jpg"
+                                    width={ 1600 }
+                                    height={ 1600 }
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                        {loaded && instanceRef.current && (
+                            <>
+                            <Arrow
+                            left
+                            onClick={(e) =>
+                                e.stopPropagation() || instanceRef.current?.prev()
+                            }
+                            disabled={currentSlide === 0}
+                            />
+
+                            <Arrow
+                            onClick={(e) =>
+                                e.stopPropagation() || instanceRef.current?.next()
+                            }
+                            disabled={
+                                currentSlide ===
+                                instanceRef.current.track.details.slides.length - 1
+                            }
+                            />
+                        </>
+                        )}
+                    </div>
+                    <div id={styles.contra_impresora}>
                         <div>
                             <Image 
                                 className= { styles.imagen }
                                 priority
-                                src="/images/un-gusto-conocerte-marca/tote-1.jpg"
+                                src="/images/mucha-mierda/contraportada.jpg"
                                 width={ 1600 }
                                 height={ 1600 }
                                 alt="Todos los estuches de conservas"
@@ -57,16 +154,55 @@ export default function MuchaMierda(){
                             <Image
                                 className= { styles.imagen }
                                 priority
-                                src="/images/un-gusto-conocerte-marca/manos.gif"
+                                src="/images/mucha-mierda/impresion.png"
                                 width={ 1600 }
                                 height={ 1600 }
                                 alt="Todos los estuches de conservas"
                                 />
                         </div>
                     </div>
+                    <Image 
+                        className= { styles.imagen }
+                        priority
+                        src="/images/mucha-mierda/revista_entera.jpg"
+                        width={ 1600 }
+                        height={ 1600 }
+                        alt="Todos los estuches de conservas"
+                    />
                 </div>
 
             </div>
         </Layout>
     )
 }
+
+function Arrow(props) {
+    const disabeld = props.disabled ? " arrow--disabled" : ""
+    return (
+    //     <svg
+    //     onClick={props.onClick}
+    //     className={`arrow ${
+    //         props.left ? "arrow--left" : "arrow--right"
+    //     }`}
+    //     xmlns="http://www.w3.org/2000/svg"
+    //     viewBox="0 0 24 24"
+    //     >
+    //     {props.left && (
+    //         <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+    //     )}
+    //     {!props.left && (
+    //       <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+    //     )}
+    //   </svg>
+        <Image
+            onClick={props.onClick}
+            priority
+            className={`arrow ${
+                props.left ? "arrow--left" : "arrow--right"
+            }`}
+            src="/images/arrow-right.svg"
+            width={ 30 }
+            height={ 30 }
+        />
+    )
+  }
